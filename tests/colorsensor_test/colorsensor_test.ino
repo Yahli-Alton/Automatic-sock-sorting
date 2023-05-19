ad7f2c929e9b51042711624a28bec84969e5033c
Yahli Alton <Yahali.elton@hrzedu.org.il>
2023-05-19 09:59:53 +0300

    print blue, green or red
    need to change the min and max input value (they aren't right) and to print the RGB color. (for checks)

--- tests/colorsensor_test/colorsensor_test.ino
+++ tests/colorsensor_test/colorsensor_test.ino
@@ -8,19 +8,24 @@ int redFrequency = 0; // The amount of red color
 int greenFrequency = 0; // The amount of green color
 int blueFrequency = 0; // the amount of blue color
 
+int red = 0;
+int blue = 0;
+int green = 0;
+
 void setup() { // run only once
   // Setting the outputs - set s0, s1, s2,s3 as the output of the colorsensor
-  pinMode(S0, INPUT);
-  pinMode(S1, INPUT);
-  pinMode(S2, INPUT);
-  pinMode(S3, INPUT);
+  pinMode(S0, OUTPUT);
+  pinMode(S1, OUTPUT);
+  pinMode(S2, OUTPUT);
+  pinMode(S3, OUTPUT);
 
   // set the sensorOut as input
   pinMode(sensorOut, INPUT);
   // Begins serial communication - the speed of the communication
   Serial.begin(9600);
 
-
+  digitalWrite(S1, HIGH);
+  digitalWrite(S0, HIGH);
 }
 
 void loop() { // run repeatedly
@@ -31,10 +36,10 @@ void loop() { // run repeatedly
   
   // Reading the output frequency
   redFrequency = pulseIn(sensorOut, LOW);
-  
+  red = map(redFrequency, 25,72,255,0);
    // Printing the RED (R) value
   Serial.print("R = ");
-  Serial.print(redFrequency);
+  Serial.print(red);
   delay(100);
   
   // Setting GREEN (G) filtered photodiodes to be read
@@ -43,10 +48,11 @@ void loop() { // run repeatedly
   
   // Reading the output frequency
   greenFrequency = pulseIn(sensorOut, LOW);
-  
+  green = map(greenFrequency, 30,90,255,0);
+  // green = map(greenFrequency, 25,70,255,0);
   // Printing the GREEN (G) value  
   Serial.print(" G = ");
-  Serial.print(greenFrequency);
+  Serial.print(green);
   delay(100);
  
   // Setting BLUE (B) filtered photodiodes to be read
@@ -55,10 +61,18 @@ void loop() { // run repeatedly
   
   // Reading the output frequency
   blueFrequency = pulseIn(sensorOut, LOW);
-  
+  blue = map(blueFrequency, 25,70,255,0);
   // Printing the BLUE (B) value 
   Serial.print(" B = ");
-  Serial.println(blueFrequency);
+  Serial.print(blue);
   delay(100);
+
+  // if (blue > 280) Serial.print(" Blue");
+  // if (red > 280) Serial.print(" Red");
+  if (blue > red && blue > green) Serial.print(" Blue");
+  if (red > blue && red > green) Serial.print(" Red");
+  if (green > red && green > blue) Serial.print(" Green");
+
+  Serial.println();
 
 }
