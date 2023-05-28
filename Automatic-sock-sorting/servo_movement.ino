3060c6b57246ce33b89b41514b1a6cb555824796
Yahli Alton <Yahali.elton@hrzedu.org.il>
2023-05-28 16:18:43 +0300

    little clean

--- Automatic-sock-sorting/servo_movement.ino
+++ Automatic-sock-sorting/servo_movement.ino
@@ -4,6 +4,69 @@
 // int color1 = 0;
 
 Servo sorterServo;
+
+
+int frequency = 0;
+
+
+
+void moveBox(int color) {
+
+   switch (color) {
+  case 1:
+   sorterServo.writeMicroseconds(1000);
+    delay(400);
+  sorterServo.writeMicroseconds(1500);
+  delay(3000);
+    sorterServo.writeMicroseconds(2000);
+        delay(350);
+          sorterServo.writeMicroseconds(1500);
+
+
+    break;
+
+     case 2:
+    sorterServo.writeMicroseconds(1000);
+     delay(750);
+  sorterServo.writeMicroseconds(1500);
+   delay(3000);
+    sorterServo.writeMicroseconds(2000);
+        delay(700);
+          sorterServo.writeMicroseconds(1500);
+
+
+     break;
+
+     case 3:
+      sorterServo.writeMicroseconds(1000);
+       delay(1050);
+  sorterServo.writeMicroseconds(1500);
+  delay(3000);
+    sorterServo.writeMicroseconds(2000);
+        delay(970);
+          sorterServo.writeMicroseconds(1500);
+    break;
+
+     case 4:
+     sorterServo.writeMicroseconds(1000);
+      delay(1350);
+  sorterServo.writeMicroseconds(1500);
+  delay(3000);
+   sorterServo.writeMicroseconds(2000);
+        delay(1300);
+          sorterServo.writeMicroseconds(1500);
+
+  
+     break;
+
+    
+    
+     case 0:
+     break;
+  }
+}
+
+
 void servomovment(){
  sorterServo.attach(3);
 
